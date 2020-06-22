6402ee4bcf7df8eb2bd183eb657a894a8b785310
Costa Kalalafatidis <costa.kal@hotmail.com>
2020-06-22 14:09:39 -0400

    2.4 exercise

--- workshop/exercises-P2_tests/exercise-2-4.js
+++ workshop/exercises-P2_tests/exercise-2-4.js
@@ -10,21 +10,33 @@
 // getLetterAtIndex("abc", 0);   // a
 
 function getLetterAtIndex(str, index) {
-  // Your code here
+  let newArr = str.charAt(index);
+  switch (str) {
+    case "":
+      return undefined;
+    default:
+      return newArr;
+  }
 }
 
-// Add 6 more test cases
-expect(getLetterAtIndex('hello', 4), 'o');
-expect(getLetterAtIndex('goodbye', 0), 'g');
+// // Add 6 more test cases
+expect(getLetterAtIndex("hello", 4), "o");
+expect(getLetterAtIndex("goodbye", 0), "g");
+expect(getLetterAtIndex("", 3), undefined);
+expect(getLetterAtIndex("10", 0), "1");
+expect(getLetterAtIndex("Hi there", 0), "H");
+expect(getLetterAtIndex("Peace", 2), "a");
+expect(getLetterAtIndex("Love", 2), "v");
+expect(getLetterAtIndex("Moomba", 1), "o");
 
-/**
- * -------------------------------------------------------------------
- * ⚠️ No changes necessary below. ⚠️
- * -------------------------------------------------------------------
- */
+// /**
+//  * -------------------------------------------------------------------
+//  * ⚠️ No changes necessary below. ⚠️
+//  * -------------------------------------------------------------------
+//  */
 function expect(result, value) {
   if (result === value) {
-    console.log('✅ Test succeeded');
+    console.log("✅ Test succeeded");
   } else {
     console.log(`⛔️ Expected “${result}” to equal “${value}”`);
   }
