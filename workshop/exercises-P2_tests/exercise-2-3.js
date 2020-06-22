eac203c0e67c4b17bcc36a4b27dc32526486b888
Costa Kalalafatidis <costa.kal@hotmail.com>
2020-06-22 13:49:35 -0400

    exercise 2.3

--- workshop/exercises-P2_tests/exercise-2-3.js
+++ workshop/exercises-P2_tests/exercise-2-3.js
@@ -9,12 +9,25 @@
 //   - `typeof 3` returns 'number')
 
 function sumDigits(input) {
-  // Your code here
+  if (
+    input.length !== 2 ||
+    typeof input[0] !== "number" ||
+    typeof input[1] !== "number"
+  ) {
+    return undefined;
+  }
+  let sum = input[0] + input[1];
+  return sum;
 }
 
 // Add 5 more test cases here! 7 total
 expect(sumDigits([2, 5]), 7);
 expect(sumDigits([-1, 0]), -1);
+expect(sumDigits([5, 7]), 12);
+expect(sumDigits([4, 10]), 14);
+expect(sumDigits([21, 4]), 25);
+expect(sumDigits([4, -2]), 2);
+expect(sumDigits([-1, -1]), -2);
 
 /**
  * -------------------------------------------------------------------
@@ -23,7 +36,7 @@ expect(sumDigits([-1, 0]), -1);
  */
 function expect(result, value) {
   if (result === value) {
-    console.log('✅ Test succeeded');
+    console.log("✅ Test succeeded");
   } else {
     console.log(`⛔️ Expected “${result}” to equal “${value}”`);
   }
