fc34b33da4555e2d443e88de76105cf4c8a71fae
Costa Kalalafatidis <costa.kal@hotmail.com>
2020-06-22 15:16:16 -0400

    exercise 2.7

--- workshop/exercises-P2_tests/exercise-2-7.js
+++ workshop/exercises-P2_tests/exercise-2-7.js
@@ -12,11 +12,35 @@
 // f(["foo", -1]) // ""
 
 function repeat(arr) {
-  // Your code here
+  let stringArr = arr[0];
+  let numberArr = arr[1];
+
+  let combo = "";
+
+  if (typeof stringArr !== "string" || typeof numberArr !== "number") {
+    return undefined;
+  }
+
+  if (numberArr <= 0) {
+    return "";
+  }
+
+  for (let i = 0; i < numberArr; i++) {
+    combo += stringArr;
+  }
+  return combo;
 }
 
 // We need 7 test cases.
 // Don't forget to test all of the question parameters
+
+expect(repeat(["foo", 3]), "foofoofoo");
+expect(repeat(["Costa ", 5]), "Costa Costa Costa Costa Costa ");
+expect(repeat(["Hello", -1]), "");
+expect(repeat([30, 1]), undefined);
+expect(repeat(["bookFace", 2]), "bookFacebookFace");
+expect(repeat(["hello", "hello"]), undefined);
+expect(repeat([40, "hello"]), undefined);
 
 /**
  * -------------------------------------------------------------------
@@ -25,7 +49,7 @@ function repeat(arr) {
  */
 function expect(result, value) {
   if (result === value) {
-    console.log('✅ Test succeeded');
+    console.log("✅ Test succeeded");
   } else {
     console.log(`⛔️ Expected “${result}” to equal “${value}”`);
   }
